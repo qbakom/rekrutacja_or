ab89278cf6360b944a8701561fdccb800c7c23da
qba <qbakom@gmail.com>
2022-08-03 17:49:49 +0200

    finished dodatkowe

--- src/dodatkowe.py
+++ src/dodatkowe.py
@@ -1,8 +1,11 @@
+from turtle import right
 import requests
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/rates/a/eur/last/100/?format=json") 
 resp = response.json()
 
 for i in range(100):
-    for x in resp["rates"][i]["mid"]:
-        print(x)
+    x = resp["rates"][i]["mid"]
+    date = resp["rates"][i]["effectiveDate"]
+    if x < 4.5 or x > 4.7:
+        print(date)
